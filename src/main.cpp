a236bfa621049d4b7a3b09eff9caa52d12388ca4
zhouqing <qing.zhou@shopee.com>
2021-03-12 14:37:43 +0800

    bo brpc-glog

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,12 @@ bool init_graph(const std::string& graph_frame_file) {
 }
 
 void init_glog(const std::string& log_path) {
-	using namespace logging;
-
-    ::logging::LoggingSettings log_setting;  // 创建LoggingSetting对象进行设置
-    log_setting.log_file = log_path.c_str(); // 设置日志路径
-    log_setting.logging_dest = logging::LOG_TO_FILE; // 设置日志写到文件，不写的话不生效
-    ::logging::InitLogging(log_setting);     // 应用日志设置
+// 	using namespace logging;
+// 
+//     ::logging::LoggingSettings log_setting;  // 创建LoggingSetting对象进行设置
+//     log_setting.log_file = log_path.c_str(); // 设置日志路径
+//     log_setting.logging_dest = logging::LOG_TO_FILE; // 设置日志写到文件，不写的话不生效
+//     ::logging::InitLogging(log_setting);     // 应用日志设置
 }
 int main(int argc, char* argv[]) {
 	gflags::ParseCommandLineFlags(&argc, &argv, true);
